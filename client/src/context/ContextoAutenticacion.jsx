4fa1cda0fcc741b6cd20643e1e099b80d63ccec4
yoquienmas <maria_dniel@yahoo.com.ar>
2026-05-19 16:07:50 -0300

    Versión recuperatorio

--- client/src/context/ContextoAutenticacion.jsx
+++
@@ -1,9 +0,0 @@
-// import { createContext } from 'react';
-
-<<<<<<< HEAD:client/src/context/ContextoAutenticacion.jsx
-// Solo el contexto
-export const ContextoAutentificacion = createContext();
-=======
-// // Solo el contexto
-// export const PaymentContext = createContext();
->>>>>>> 438881489b1d7325c4f50de9f3073f1942fb116a:client/src/context/PaymentContext.jsx
